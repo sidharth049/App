c2481700382a236da3d1454ec7fe29fb1a15edf2
sidharth049 <sidhu049@gmail.com>
2016-12-13 00:46:03 +0530

    corrected

--- App.Repositories/Repositories/RepositoryBase.cs
+++ App.Repositories/Repositories/RepositoryBase.cs
@@ -33,7 +33,7 @@ namespace App.Repositories
 
         public virtual void Add(T entity)
         {
-            dbSet.Add(T);
+            dbSet.Add(entity);
         }
 
         public virtual void Update(T entity)
